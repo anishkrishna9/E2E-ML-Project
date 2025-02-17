3f0347980edbc457847d527340a42fe31a505963
anishkrishna9 <anishkrishna9@gmail.com>
2025-02-17 19:25:32 +0530

    Logger Added

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-from src.WineQualityProject.config import configuration
+#from src.WineQualityProject.config import configuration
+from WineQualityProject import logger
+logger.info("Hello, I am Anish Krishna")
