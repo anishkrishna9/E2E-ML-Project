61bb8efa54eca3ad64b2a72b10ef523dca6bba6b
anishkrishna9 <anishkrishna9@gmail.com>
2025-02-28 11:58:18 +0530

    data transformation added

--- src/WineQualityProject/config/configuration.py
+++ src/WineQualityProject/config/configuration.py
@@ -1,7 +1,6 @@
 from WineQualityProject.constants import *
 from WineQualityProject.utils.common import read_yaml, create_directories
-from WineQualityProject.entity.config_entity import DataIngestionConfig
-from WineQualityProject.entity.config_entity import DataValidationConfig
+from WineQualityProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -44,4 +43,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
 
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
