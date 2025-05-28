ded966cee541d4b58871f4e1794171e1036b65ae
anishkrishna9 <anishkrishna9@gmail.com>
2025-05-28 22:55:59 +0530

    user add added

---
+++ main.py
@@ -0,0 +1,57 @@
+from WineQualityProject import logger
+from WineQualityProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from WineQualityProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from WineQualityProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from WineQualityProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from WineQualityProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/WineQualityProject/components/data_validation.py
@@ -0,0 +1,33 @@
+import os
+from WineQualityProject import logger
+import pandas as pd
+from WineQualityProject.entity.config_entity import DataValidationConfig
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    
+    def validate_all_columns(self)-> bool:
+        try:
+            validation_status = True
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+            if 'Id' in data.columns:
+                data = data.drop(columns=['Id'])
+            all_cols = list(data.columns)
+
+            all_schema = self.config.all_schema.keys()
+            
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    break
+            
+            with open(self.config.STATUS_FILE, 'w') as f:
+                f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+        
+        except Exception as e:
+            raise e
